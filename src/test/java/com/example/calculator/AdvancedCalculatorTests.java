d15428e608f521adbb5a8ceb62c040ae139d8675
Tony Mowers <tony@amowers.com>
2022-05-03 13:30:40 +0200

    parameterized advance calculator tests
    fixed typo
    
    
    parameterized distance tests
    
    
    added a new distance test

--- src/test/java/com/example/calculator/AdvancedCalculatorTests.java
+++ src/test/java/com/example/calculator/AdvancedCalculatorTests.java
@@ -4,8 +4,11 @@ import com.example.calculator.helpers.AdvancedCalculatorUser;
 import com.example.calculator.helpers.Point;
 import com.example.calculator.helpers.Triangle;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.closeTo;
@@ -13,11 +16,18 @@ import static org.hamcrest.Matchers.is;
 
 public class AdvancedCalculatorTests {
 
-    public static final Triangle THREE_FOUR_FIVE_TRIANGLE = new Triangle(
+    private static final Triangle THREE_FOUR_FIVE_RIGHT_TRIANGLE = new Triangle(
             new Point(0, 0),
             new Point(3, 4),
             new Point(3, 0)
     );
+
+    private static final Triangle ONE_BY_ONE_RIGHT_TRIANGLE = new Triangle(
+            new Point(1, 1),
+            new Point(2, 2),
+            new Point(2, 1)
+    );
+
     private AdvancedCalculatorUser user;
 
     @BeforeEach
@@ -25,26 +35,47 @@ public class AdvancedCalculatorTests {
         user = new AdvancedCalculatorUser();
     }
 
-    @Test
-    public void test_distance_between_points() {
-        Point pointA = new Point(0, 0);
-        Point pointB = new Point(3, 4);
-
+    @ParameterizedTest
+    @MethodSource
+    public void test_distance_between_points(Point pointA, Point pointB, double expectedDistance) {
         user.calculatesDistanceBetweenPoints(pointA, pointB);
 
-        assertUserSeesResult(5);
+        assertUserSeesResult(expectedDistance);
     }
 
-    @Test
-    public void test_perimeter_of_triangle() {
-        user.calculatesPerimeterOfTriangle(THREE_FOUR_FIVE_TRIANGLE);
-        assertUserSeesResult(3 + 4 + 5);
+    @ParameterizedTest
+    @MethodSource
+    public void test_perimeter_of_triangle(Triangle triangle, double expectedPerimeter) {
+        user.calculatesPerimeterOfTriangle(triangle);
+        assertUserSeesResult(expectedPerimeter);
     }
 
-    @Test
-    void test_area_of_triangle() {
-        user.calculatesAreaOfTriangle(THREE_FOUR_FIVE_TRIANGLE);
-        assertUserSeesResult( 6.0);
+    @ParameterizedTest
+    @MethodSource
+    void test_area_of_triangle(Triangle triangle, double expectedArea) {
+        user.calculatesAreaOfTriangle(triangle);
+        assertUserSeesResult( expectedArea);
+    }
+
+    private static Stream<Arguments> test_distance_between_points() {
+        return Stream.of(
+                Arguments.of(new Point(0, 0), new Point(3, 4), 5.0),
+                Arguments.of(new Point(10, 10), new Point(40, 50), 50.0)
+        );
+    }
+
+    private static Stream<Arguments> test_perimeter_of_triangle() {
+        return Stream.of(
+                Arguments.of(THREE_FOUR_FIVE_RIGHT_TRIANGLE, 3 + 4 + 5),
+                Arguments.of(ONE_BY_ONE_RIGHT_TRIANGLE, 1 + 1 + Math.sqrt(2.0))
+        );
+    }
+
+    private static Stream<Arguments> test_area_of_triangle() {
+        return Stream.of(
+                Arguments.of(THREE_FOUR_FIVE_RIGHT_TRIANGLE, 3 * 4 / 2.0),
+                Arguments.of(ONE_BY_ONE_RIGHT_TRIANGLE, 1.0 * 1 / 2.0)
+        );
     }
 
     private void assertUserSeesResult(double value) {
